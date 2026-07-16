d0f25f55139e1cfbfb5c9b9d7113fe72f962318e
vhieudzai <bantienhoa00@gmail.com>
2026-07-16 17:37:19 +0700

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "vi hieu"
+getgenv().ScriptSubTitle = "by vi hieu"
+getgenv().ScriptImage = "https://i.ibb.co/mCZTWFJ9/vi-hi-u.jpg"
+getgenv().ScriptAuthorName = "vi hiếu"
+getgenv().ScriptAuthorSubTitle = "https://discord.gg/DzmYysnRN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
